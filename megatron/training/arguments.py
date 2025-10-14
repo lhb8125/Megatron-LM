157bf2fcabf5c3ef32c5cd6d9b56cecd4395dfec
Hongbin Liu <hongbinl@nvidia.com>
2025-10-13 18:38:22 -0700

    add TE version control
    Signed-off-by: Hongbin Liu <hongbinl@nvidia.com>

--- megatron/training/arguments.py
+++ megatron/training/arguments.py
@@ -1190,8 +1190,9 @@ def validate_args(args, defaults={}):
     if args.fine_grained_activation_offloading:
         assert args.transformer_impl == 'transformer_engine', \
             "Fine-grained activation offloading is only supported with transformer_engine implementation"
-        assert is_te_min_version("2.8.0"), \
-            "Fine-grained activation offloading is only supported with TE >= 2.8.0"
+        assert is_te_min_version("2.9.0"), \
+            "Fine-grained activation offloading is only supported with TE >= 2.9.0." \
+            "For EA test, please checkout to TE PR #2267 and comment out this assert"
 
     if args.mtp_num_layers:
         assert not args.use_legacy_models, "The legacy Megatron models does not support Multi-Token Prediction (MTP)."
